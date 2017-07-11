40a9e0799844dc4c719aeff633e94a4da43a2b2d
PhanVanLinh <phanvanlinh.94vn@gmail.com>
2017-07-11 16:03:44 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# AndroidUsingGooglePlace
+
+Follow here: https://developers.google.com/places/android-api/start
+
+#### Step 1: Get a key
+#### Step 2: Add meta data to AndroidManifest
+#### Step 3: Add fragment to xml or open new activity (https://developers.google.com/places/android-api/autocomplete)
---
+++ app/src/main/java/com/toong/androidusinggoogleplace/MainActivity.java
@@ -0,0 +1,60 @@
+package com.toong.androidusinggoogleplace;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocomplete;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    private GoogleApiClient mGoogleApiClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        PlaceAutocompleteFragment fragment =
+                (PlaceAutocompleteFragment) getFragmentManager().findFragmentById(
+                        R.id.place_autocomplete_fragment);
+
+        fragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                // TODO: Get info about the selected place.
+                Log.i(TAG, "Place: " + place.getName());
+            }
+
+            @Override
+            public void onError(Status status) {
+                // TODO: Handle the error.
+                Log.i(TAG, "An error occurred: " + status);
+            }
+        });
+
+        final int PLACE_AUTOCOMPLETE_REQUEST_CODE = 1;
+        findViewById(R.id.select_place_activity).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent intent = new PlaceAutocomplete.IntentBuilder(
+                            PlaceAutocomplete.MODE_FULLSCREEN).build(MainActivity.this);
+                    startActivityForResult(intent, PLACE_AUTOCOMPLETE_REQUEST_CODE);
+                } catch (GooglePlayServicesRepairableException e) {
+                    // TODO: Handle the error.
+                } catch (GooglePlayServicesNotAvailableException e) {
+                    // TODO: Handle the error.
+                }
+            }
+        });
+    }
+}
